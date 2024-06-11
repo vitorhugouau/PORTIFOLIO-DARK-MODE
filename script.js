52c14c649a123be00140e8a1595c41fabc4d21c9
vitorhugouau <vitorhugkrodrigues@gmail.com>
2024-06-11 19:41:05 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const menu = document.querySelector(".links-menu");
+  const icon = document.querySelector(".icone-hamburger");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
